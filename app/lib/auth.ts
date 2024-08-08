0f67d1e6ba328c9a4a777a6adbb8386e4258e98e
Parikshit <parikshitgupta2001@gmail.com>
2024-08-08 23:12:38 +0530

    Added auth logic to store data in db while logging in

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,11 +1,90 @@
-import { AuthOptions } from "next-auth";
+import { AuthOptions, Session } from "next-auth";
 import Google from "next-auth/providers/google";
+import prisma from "../db";
+import { Keypair } from "@solana/web3.js";
+
+export interface session extends Session {
+  user: {
+    email: string;
+    image: string;
+    name: string;
+    uid: string;
+  };
+}
 
 export const authConfig: AuthOptions = {
+  secret: process.env.NEXT_AUTH_SECRET || "secr3t",
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
+  callbacks: {
+    signIn: async ({ user, account, profile }: any) => {
+      if (account.provider !== "google") {
+        return false;
+      }
+
+      const email: string = user.email;
+      if (!email) {
+        return false;
+      }
+
+      const userDB = await prisma.user.findFirst({
+        where: {
+          username: email,
+        },
+      });
+      if (userDB) {
+        return true;
+      }
+
+      const keypair = Keypair.generate();
+      const publicKey = keypair.publicKey.toBase58();
+      const privateKey = keypair.secretKey;
+
+      await prisma.user.create({
+        data: {
+          username: email,
+          sub: account.providerAccountId,
+          name: user.name,
+          profilePicture: user.image,
+          provider: "Google",
+          solWallet: {
+            create: {
+              publicKey: publicKey,
+              privateKey: privateKey.toString(),
+            },
+          },
+          inrWallet: {
+            create: {
+              balance: 0,
+            },
+          },
+        },
+      });
+
+      return true;
+    },
+    async jwt({ token, account }: any) {
+      const user = await prisma.user.findFirst({
+        where: {
+          sub: token.sub ?? "",
+        },
+      });
+      if (user) {
+        token.uid = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }: any) {
+      // Send properties to the client, like an access_token and user id from a provider.
+      const newSession: session = session as session;
+      if (newSession.user && token.uid) {
+        newSession.user.uid = token.uid;
+      }
+      return newSession;
+    },
+  },
 };
