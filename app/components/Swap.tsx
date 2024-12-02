a4956eb51e8dc6edf9901d32f31af7ad2f9a87f3
Parikshit <parikshitgupta2001@gmail.com>
2024-12-02 14:06:51 +0530

    Fix some DOM errors

--- app/components/Swap.tsx
+++ app/components/Swap.tsx
@@ -183,11 +183,7 @@ function HeroLoading() {
       stroke="currentColor"
       className="size-6"
     >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
-      />
+      <path d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
     </svg>
   );
 }
@@ -229,15 +225,10 @@ function SwapIcon() {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      stroke-width="1.5"
       stroke="currentColor"
       className="size-6"
     >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5"
-      />
+      <path d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
     </svg>
   );
 }
